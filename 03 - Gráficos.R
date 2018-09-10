9f8ff94c31c653735ed62190a7e61b82f3cc2bba
IgorNascimento <igor.ferreira.n@gmail.com>
2018-09-10 15:34:33 -0300

    ok

--- 03 - Gráficos.R
+++ 03 - Gráficos.R
@@ -9,18 +9,15 @@ arquivos <- list.files(pattern = "retornos_.*rds",
 i <- 1
 for(i in 1:length(arquivos)){
 base <- readRDS(arquivos[i])
-
+names(base)[grep("^RMT$",names(base))] <- "pearson_RMT"
 base <- base %>% gather(key = "variable" ,
                         value = "valor",
                         -DT)
+
+
 # gráfico KPCA ----
 base$type_line <- ifelse(grepl("_RMT",base$variable),"dotted","solid")
 base$variable2 <- gsub('_RMT',"",base$variable)
-
-# base <- base %>% select(variable2,DT,valor,type_line) %>% 
-#   spread(key = "type_line" ,
-#                         value = "valor")
-
 
 g <- ggplot(data = base, 
        aes(x = DT, y = valor, color = variable2))+
@@ -32,22 +29,81 @@ g <- ggplot(data = base,
   xlab('Time') +
   ylab('Cumulative Return') +
   scale_colour_manual(name = "",
-                      values = c("pearson"="red4","RMT"="turquoise1",
-                                 "GAUSS"="green","MCD"="blue",
-                                 "OGK"="red1","WMCD"="purple1",
-                                 "POLY2"="orange","POLY3"="khaki4",
-                                 "POLY4"="skyblue1"),
-                      labels = c("Pearson","RMT",
-                                 "Gauss","MCD",
-                                 "OGK","WMCD",
+                      values = c("GAUSS"="green",
+                                 "MCD"="blue",
+                                 "OGK"="red1",
+                                 "pearson"="red4",
+                                 "POLY2"="orange",
+                                 "POLY3"="pink",
+                                 "POLY4"="skyblue1",
+                                 "WMCD"="purple1"),
+                      labels = c("Gauss","MCD",
+                                 "OGK","Pearson",
                                  "Poly2","Poly3",
-                                 "Poly4"))+
+                                 "Poly4",
+                                 "WMCD"))+
   scale_linetype_manual(name = "",
                         values = c("solid"="solid",
                                    "dotted"="dotted"),
-                        labels = c("Filtered","No-filtered"))
+                        labels = c("Filtered","Non-filtered"))
 ggsave(paste0("graficos/grafico_",gsub(".*retornos_(.*).rds","\\1",arquivos[i]),".jpg"),
        device = "jpg",width = 12, height = 8)
 ggsave(paste0("graficos/grafico_",gsub(".*retornos_(.*).rds","\\1",arquivos[i]),".pdf"),
        device = "pdf",width = 12, height = 8)
+
+
+
+base <- base %>% select(DT,variable2,type_line,valor) %>% 
+  spread (key = "type_line" , value = "valor")
+
+g <- ggplot(data = base, 
+            aes(x = DT, y = (dotted-solid), color = variable2))+
+  geom_line(size=.9) + 
+  scale_y_continuous(labels = scales::percent) +
+  theme_classic() +
+  theme(legend.position = "bottom") +
+  xlab('Time') +
+  ylab('Cumulative return on filtered approach') +
+  scale_colour_manual(name = "",
+                      values = c("GAUSS"="green",
+                                 "MCD"="blue",
+                                 "OGK"="red1",
+                                 "pearson"="red4",
+                                 "POLY2"="orange",
+                                 "POLY3"="pink",
+                                 "POLY4"="skyblue1",
+                                 "WMCD"="purple1"),
+                      labels = c("Gauss","MCD",
+                                 "OGK","Pearson",
+                                 "Poly2","Poly3",
+                                 "Poly4",
+                                 "WMCD"))
+ggsave(paste0("graficos/relativo_grafico_",gsub(".*retornos_(.*).rds","\\1",arquivos[i]),".jpg"),
+       device = "jpg",width = 12, height = 8)
+ggsave(paste0("graficos/relativo_grafico_",gsub(".*retornos_(.*).rds","\\1",arquivos[i]),".pdf"),
+       device = "pdf",width = 12, height = 8)
+
+g <- ggplot(data = base %>% filter(!grepl("POLY",variable2)), 
+            aes(x = DT, y = (dotted-solid), color = variable2))+
+  geom_line(size=.9) + 
+  scale_y_continuous(labels = scales::percent) +
+  theme_classic() +
+  theme(legend.position = "bottom") +
+  xlab('Time') +
+  ylab('Cumulative Return') +
+  scale_colour_manual(name = "",
+                      values = c("GAUSS"="green",
+                                 "MCD"="blue",
+                                 "OGK"="red1",
+                                 "pearson"="red4",
+                                 "WMCD"="purple1"),
+                      labels = c("Gauss","MCD",
+                                 "OGK","Pearson",
+                                 "WMCD"))
+ggsave(paste0("graficos/relativo_sem_poly_grafico_",gsub(".*retornos_(.*).rds","\\1",arquivos[i]),".jpg"),
+       device = "jpg",width = 12, height = 8)
+ggsave(paste0("graficos/relativo_sem_poly_grafico_",gsub(".*retornos_(.*).rds","\\1",arquivos[i]),".pdf"),
+       device = "pdf",width = 12, height = 8)
+
+print(i)
 }
